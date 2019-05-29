1326750d2ff427929e9144e7d97c5daf3f517c00
DennySPB <dennyspb@gmail.com>
2019-05-29 09:55:50 +0300

    Revert "binder: Release tracking lock before invoking binder_proxy_limit_callback"
    This reverts commit 2ca697e959364ca5d750857fb082b104e479bb72.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- libs/binder/BpBinder.cpp
+++ libs/binder/BpBinder.cpp
@@ -110,11 +110,10 @@ BpBinder* BpBinder::create(int32_t handle) {
     int32_t trackedUid = -1;
     if (sCountByUidEnabled) {
         trackedUid = IPCThreadState::self()->getCallingUid();
-        sTrackingLock.lock();
+        AutoMutex _l(sTrackingLock);
         uint32_t trackedValue = sTrackingMap[trackedUid];
         if (CC_UNLIKELY(trackedValue & LIMIT_REACHED_MASK)) {
             if (sBinderProxyThrottleCreate) {
-                sTrackingLock.unlock();
                 return nullptr;
             }
         } else {
@@ -122,21 +121,16 @@ BpBinder* BpBinder::create(int32_t handle) {
                 ALOGE("Too many binder proxy objects sent to uid %d from uid %d (%d proxies held)",
                       getuid(), trackedUid, trackedValue);
                 sTrackingMap[trackedUid] |= LIMIT_REACHED_MASK;
-                sTrackingLock.unlock();
-                // Release sTrackingLock before calling into BinderProxy, or we might end in dead lock
                 if (sLimitCallback) sLimitCallback(trackedUid);
-                sTrackingLock.lock();
                 if (sBinderProxyThrottleCreate) {
                     ALOGI("Throttling binder proxy creates from uid %d in uid %d until binder proxy"
                           " count drops below %d",
                           trackedUid, getuid(), sBinderProxyCountLowWatermark);
                     return nullptr;
-                    sTrackingLock.unlock();
                 }
             }
         }
         sTrackingMap[trackedUid]++;
-        sTrackingLock.unlock();
     }
     return new BpBinder(handle, trackedUid);
 }
