2bed8677d91868382df20fcf3bdc6fa0a69dfe9a
Ramakant Singh <ramakant@codeaurora.org>
2020-09-24 13:24:15 +0300

    sf: Allow VDS to use HWC
    -- Preserve VDS layer pixel format based
       on GRALLOC flags.
    -- skip color layer for vds
    
     CRs-Fixed: 2178626
    
    DennySPb: bring to A11
    
    Change-Id: I36d0fe6edb72732a9fe5d2e66ca2d95f3ef7e1dc
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -242,6 +242,8 @@ public:
 
     static bool useContextPriority;
 
+    static bool sDirectStreaming;
+
     // The data space and pixel format that SurfaceFlinger expects hardware composer
     // to composite efficiently. Meaning under most scenarios, hardware composer
     // will accept layers with the data space and pixel format.
@@ -738,6 +740,8 @@ private:
     void traverseLayersInDisplay(const sp<const DisplayDevice>& display,
                                  const LayerVector::Visitor& visitor);
 
+    bool canAllocateHwcDisplayIdForVDS(uint64_t usage);
+    bool skipColorLayer(const char* layerType);
     sp<StartPropertySetThread> mStartPropertySetThread;
 
     /* ------------------------------------------------------------------------
