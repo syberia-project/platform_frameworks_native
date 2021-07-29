5cb53bf9863cc6635d6c661e2ce390b55153d5d9
rnlee <rnlee@google.com>
2021-07-29 13:58:30 -0700

    Add FlagManager instance to SF.
    Bug: 190769260
    Test: make;
    
    1. Examine the value returned by FlagManager:
       `adb shell dumpsys SurfaceFlinger | grep -A 10 "FlagManager"`
    2. Examine the value returned by P/H:
       `adb shell device_config get surface_flinger_native_boot
       DemoFeature__demo_flag`
    
    Change-Id: Ied375fa732c33be31cc5e1c6dcdfb024aecb3e0d
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -89,6 +89,7 @@ namespace android {
 
 class Client;
 class EventThread;
+class FlagManager;
 class FpsReporter;
 class TunnelModeEnabledReporter;
 class HdrLayerInfoReporter;
@@ -1521,6 +1522,8 @@ private:
     wp<IBinder> mActiveDisplayToken GUARDED_BY(mStateLock);
 
     const sp<WindowInfosListenerInvoker> mWindowInfosListenerInvoker;
+
+    std::unique_ptr<FlagManager> mFlagManager;
 };
 
 } // namespace android
