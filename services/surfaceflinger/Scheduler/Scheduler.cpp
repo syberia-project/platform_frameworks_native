674ef4176038c3f827cac87d71b586eaf7e63df3
Juhyung Park <qkrwngud825@gmail.com>
2022-05-23 08:13:38 +0900

    SF: Scheduler: vote max for NOTIFICATION_SHADE
    We want the maximum refresh rate for pulling down the notification panel.
    
    str: sync with kcontent detection changes
    
    Change-Id: I688f6f07089f1db9d39f2c33e89239480c0c782a
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/surfaceflinger/Scheduler/Scheduler.cpp
+++ services/surfaceflinger/Scheduler/Scheduler.cpp
@@ -499,6 +499,9 @@ void Scheduler::registerLayer(Layer* layer) {
     if (!mFeatures.test(Feature::kContentDetection) ||
         layer->getWindowType() == WindowType::STATUS_BAR) {
         voteType = scheduler::LayerHistory::LayerVoteType::NoVote;
+    } else if (layer->getWindowType() == WindowType::NOTIFICATION_SHADE) {
+        // Enforce max refresh rate for notification pulldown
+        voteType = scheduler::LayerHistory::LayerVoteType::Max;
     } else if (layer->getWindowType() == WindowType::WALLPAPER) {
         // Running Wallpaper at Min is considered as part of content detection.
         voteType = scheduler::LayerHistory::LayerVoteType::Min;
