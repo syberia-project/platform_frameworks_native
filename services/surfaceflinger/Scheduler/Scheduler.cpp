AOSPA/android_frameworks_native

SF: Scheduler: add more window types to consider NoVote
These types don't need high refresh rate.

Change-Id: I5170cbb7ed5de8c97d8e25d08a567af4250aef86
Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

services/surfaceflinger/Scheduler/Scheduler.cpp
@@ -608,10 +608,22 @@ void Scheduler::setIgnorePresentFences(bool ignore) {
 
 void Scheduler::registerLayer(Layer* layer) {
     scheduler::LayerHistory::LayerVoteType voteType;
+    const auto windowType = layer->getWindowType();
 
-    if (!mOptions.useContentDetection || layer->getWindowType() == WindowInfo::Type::STATUS_BAR) {
+    if (!mOptions.useContentDetection ||
+        windowType == WindowInfo::Type::STATUS_BAR ||
+        windowType == WindowInfo::Type::SYSTEM_ALERT ||
+        windowType == WindowInfo::Type::TOAST ||
+        windowType == WindowInfo::Type::SYSTEM_DIALOG ||
+        windowType == WindowInfo::Type::KEYGUARD_DIALOG ||
+        windowType == WindowInfo::Type::INPUT_METHOD ||
+        windowType == WindowInfo::Type::INPUT_METHOD_DIALOG ||
+        windowType == WindowInfo::Type::NAVIGATION_BAR ||
+        windowType == WindowInfo::Type::VOLUME_OVERLAY ||
+        windowType == WindowInfo::Type::NAVIGATION_BAR_PANEL ||
+        windowType == WindowInfo::Type::NOTIFICATION_SHADE) {
         voteType = scheduler::LayerHistory::LayerVoteType::NoVote;
-    } else if (layer->getWindowType() == WindowInfo::Type::WALLPAPER) {
+    } else if (windowType == WindowInfo::Type::WALLPAPER) {
         // Running Wallpaper at Min is considered as part of content detection.
         voteType = scheduler::LayerHistory::LayerVoteType::Min;
     } else {

syberia-project/platform_frameworks_native

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: DennySPb <dennyspb@gmail.com>

services/surfaceflinger/Scheduler/Scheduler.cpp
@@ -581,10 +581,21 @@ void Scheduler::setIgnorePresentFences(bool ignore) {
 
 void Scheduler::registerLayer(Layer* layer) {
     scheduler::LayerHistory::LayerVoteType voteType;
+    const auto windowType = layer->getWindowType();
 
-    if (!mOptions.useContentDetection || layer->getWindowType() == WindowInfo::Type::STATUS_BAR) {
+    if (!mOptions.useContentDetection ||
+        windowType == WindowInfo::Type::STATUS_BAR ||
+        windowType == WindowInfo::Type::SYSTEM_ALERT ||
+        windowType == WindowInfo::Type::TOAST ||
+        windowType == WindowInfo::Type::SYSTEM_DIALOG ||
+        windowType == WindowInfo::Type::KEYGUARD_DIALOG ||
+        windowType == WindowInfo::Type::INPUT_METHOD ||
+        windowType == WindowInfo::Type::INPUT_METHOD_DIALOG ||
+        windowType == WindowInfo::Type::NAVIGATION_BAR ||
+        windowType == WindowInfo::Type::VOLUME_OVERLAY ||
+        windowType == WindowInfo::Type::NAVIGATION_BAR_PANEL) {
         voteType = scheduler::LayerHistory::LayerVoteType::NoVote;
-    } else if (layer->getWindowType() == WindowInfo::Type::WALLPAPER) {
+    } else if (windowType == WindowInfo::Type::WALLPAPER) {
         // Running Wallpaper at Min is considered as part of content detection.
         voteType = scheduler::LayerHistory::LayerVoteType::Min;
     } else {