40f35951ba01321a279d3475f6d6256d3fee6852
chawan <quic_chawan@quicinc.com>
2023-05-15 17:19:26 +0800

    Update PhoneLink 14-3 frameworks/native change
    Change-Id: I6bbdc37ad853ad07a779933ec30275af9c207327
    CRs-Fixed：3498735
    
    Change-Id: I443da086a80c7018a7d280b7cb46e9d00729cc5c

--- services/inputflinger/dispatcher/InputDispatcher.h
+++ services/inputflinger/dispatcher/InputDispatcher.h
@@ -246,9 +246,16 @@ private:
     // to transfer focus to a new application.
     std::shared_ptr<const EventEntry> mNextUnblockedEvent GUARDED_BY(mLock);
 
+#ifdef DISABLE_DEVICE_INTEGRATION
     sp<android::gui::WindowInfoHandle> findTouchedWindowAtLocked(
             int32_t displayId, float x, float y, bool isStylus = false,
             bool ignoreDragWindow = false) const REQUIRES(mLock);
+#else
+    // Device Integration: add a new param into this method
+    sp<android::gui::WindowInfoHandle> findTouchedWindowAtLocked(
+            int32_t displayId, float x, float y, bool isStylus = false,
+            bool ignoreDragWindow = false, bool isFromCrossDevice = false) const REQUIRES(mLock);
+#endif
     std::vector<InputTarget> findOutsideTargetsLocked(
             int32_t displayId, const sp<android::gui::WindowInfoHandle>& touchedWindow,
             int32_t pointerId) const REQUIRES(mLock);
@@ -554,7 +561,13 @@ private:
     // Enqueue a drag event if needed, and update the touch state.
     // Uses findTouchedWindowTargetsLocked to make the decision
     void addDragEventLocked(const MotionEntry& entry) REQUIRES(mLock);
+
+#ifdef DISABLE_DEVICE_INTEGRATION
     void finishDragAndDrop(int32_t displayId, float x, float y) REQUIRES(mLock);
+#else
+    // Device Integration: add a new param into this method
+    void finishDragAndDrop(int32_t displayId, float x, float y, bool isFromCrossDevice = false) REQUIRES(mLock);
+#endif
 
     struct TouchOcclusionInfo {
         bool hasBlockingOcclusion;
